ee3f24862df00289976a9d6d6b08bd69bf63c890
SmileDoge <vladsavchuk2006@gmail.com>
2020-10-02 00:10:14 +0300

    update

---
+++ Assets/Networking/MainNetwork.cs
@@ -0,0 +1,49 @@
+﻿using NativeWebSocket;
+using Networking.Packets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Networking
+{
+    class MainNetwork
+    {
+
+        public delegate void PacketHandler(Packet packet);
+
+        private static WebSocket _websocket;
+        private static Dictionary<int, PacketHandler> _packetHandlers = new Dictionary<int, PacketHandler>();
+
+        public static async void Connect(string url)
+        {
+            _websocket = new WebSocket(url);
+
+            _websocket.OnMessage += HandleMessage;
+
+            await _websocket.Connect();
+        }
+
+        private static void InitializePackets()
+        {
+            _packetHandlers = new Dictionary<int, PacketHandler>()
+            {
+                { (int)PacketEnum.SERVER_JOIN_ROOM, ClientHandle.JoinRoom},
+                { (int)PacketEnum.SERVER_LEAVE_ROOM, ClientHandle.JoinRoom},
+            };
+        }
+
+        private static void HandleMessage(byte[] data)
+        {
+            using ( var packet = new Packet(data) )
+            {
+                var packet_id = packet.ReadInt();
+                if ( _packetHandlers.TryGetValue(packet_id, out PacketHandler value))
+                {
+                    _packetHandlers[packet_id](packet);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Network.cs
@@ -0,0 +1,24 @@
+﻿using NativeWebSocket;
+using UnityEngine;
+
+public class Network : MonoBehaviour
+{
+    private WebSocket _socket;
+    
+    async void Start()
+    {
+        _socket = new WebSocket("ws://localhost:7000");
+
+        _socket.OnOpen += () =>
+        {
+            Debug.Log("Connected");
+        };
+
+        await _socket.Connect();
+    }
+    
+    void Update()
+    {
+        
+    }
+}
